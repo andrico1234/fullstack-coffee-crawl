2dbcbd939b30c1c8aa01a066deb4a5e03f381357
Andrico <andrico1234@yahoo.co.uk>
2017-10-24 08:20:41 +0100

    improve generic page and error page UI add favicon

--- client/src/components/error_page/error_page.js
+++ client/src/components/error_page/error_page.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 class ErrorPage extends Component {
     render() {
+        const windowHeight = window.innerHeight - 213;
+
         return(
-            <div className="generic-page-wrapper">
-                <h1>404</h1>
-                <h2>Page Not Found</h2>
-                <pre>Line x of file y</pre>
+            <div className="generic-page-wrapper" style={{minHeight: windowHeight + 'px'}}>
+                <h1 className="title">404</h1>
+                <h2 className="subtitle">Page Not Found</h2>
+                <p className="content">Line x of file y</p>
             </div>
         );
     }
